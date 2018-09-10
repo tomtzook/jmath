7d6c46770d6227c88a18be8d2c90de7ac741ca6c
Tom Tzook <tomtzook@gmail.com>
2018-09-10 18:30:37 +0300

    fixed mispell

--- src/main/java/com/jmath/Mathx.java
+++ src/main/java/com/jmath/Mathx.java
@@ -6,19 +6,19 @@ public class Mathx {
 
     private static final double ROOT_DIFFERENCE = 1e-8;
 
-    public static double root(double redicand, int degree){
-        if(redicand < 0) {
-            throw new IllegalArgumentException("Cannot calculate negative root of negative redicand");
+    public static double root(double radicand, int degree){
+        if(radicand < 0) {
+            throw new IllegalArgumentException("Cannot calculate negative root of negative radicand");
         }
-        if(redicand == 0) {
+        if(radicand == 0) {
             return 0;
         }
 
-        double x1 = redicand;
-        double x2 = redicand / degree;
+        double x1 = radicand;
+        double x2 = radicand / degree;
         while (Math.abs(x1 - x2) > ROOT_DIFFERENCE){
             x1 = x2;
-            x2 = ((degree - 1.0) * x2 + redicand / Math.pow(x2, degree - 1.0)) / degree;
+            x2 = ((degree - 1.0) * x2 + radicand / Math.pow(x2, degree - 1.0)) / degree;
         }
 
         return x2;
