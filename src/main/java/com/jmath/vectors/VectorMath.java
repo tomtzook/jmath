0accda407e8b5f3ce8cd64459d2d91f39cc9ac15
Tom Tzook <tomtzook@gmail.com>
2018-09-10 17:45:41 +0300

    added vector math static class

---
+++ src/main/java/com/jmath/vectors/VectorMath.java
@@ -0,0 +1,30 @@
+package com.jmath.vectors;
+
+public class VectorMath {
+
+    public static Vector2 net(Vector2... vectors) {
+        double x = 0;
+        double y = 0;
+
+        for (Vector2 vector : vectors) {
+            x += vector.x();
+            y += vector.y();
+        }
+
+        return new Vector2(x, y);
+    }
+
+    public static Vector3 net(Vector3... vectors) {
+        double x = 0;
+        double y = 0;
+        double z = 0;
+
+        for (Vector3 vector : vectors) {
+            x += vector.x();
+            y += vector.y();
+            z += vector.z();
+        }
+
+        return new Vector3(x, y, z);
+    }
+}
