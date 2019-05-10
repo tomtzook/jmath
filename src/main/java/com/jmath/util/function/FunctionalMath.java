6da3b0e4edb38d06804221a213cba894dadde57a
Tom Tzook <tomtzook@gmail.com>
2019-05-10 21:08:05 +0300

    added additional functional math

--- src/main/java/com/jmath/util/function/FunctionalMath.java
+++ src/main/java/com/jmath/util/function/FunctionalMath.java
@@ -16,4 +16,40 @@ public class FunctionalMath {
     public static DoublePredicate constrained(double min, double max) {
         return (value) -> ExtendedMath.constrained(value, min, max);
     }
+
+    public static DoublePredicate largerThan(double value) {
+        return (someValue) -> someValue > value;
+    }
+
+    public static DoublePredicate largerOrEqualsTo(double value) {
+        return (someValue) -> someValue >= value;
+    }
+
+    public static DoublePredicate absLargerThan(double value) {
+        return (someValue) -> Math.abs(someValue) > value;
+    }
+
+    public static DoublePredicate absLargerOrEqualsTo(double value) {
+        return (someValue) -> Math.abs(someValue) >= value;
+    }
+
+    public static DoublePredicate smallerThan(double value) {
+        return (someValue) -> someValue < value;
+    }
+
+    public static DoublePredicate smallerEqualsTo(double value) {
+        return (someValue) -> someValue <= value;
+    }
+
+    public static DoublePredicate absSmallerThan(double value) {
+        return (someValue) -> Math.abs(someValue) < value;
+    }
+
+    public static DoublePredicate absSmallerOrEqualsTo(double value) {
+        return (someValue) -> Math.abs(someValue) <= value;
+    }
+
+    public static DoublePredicate equalsTo(double value) {
+        return (someValue) -> someValue == value;
+    }
 }
